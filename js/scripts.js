8c8f5d3d70e9bf3f9d1de50968e6e0d9ccb468fd
xenogonx <pablez@wp.pl>
2017-04-17 20:34:39 +0200

    population change

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ function showCountriesList(resp) {
     $('<h3>').text(item.name).appendTo(countriesList);
     $('<img>').attr('src', '' + 'http://www.geonames.org/flags/x/' + item.alpha2Code.toLowerCase() + ".gif").appendTo(countriesList);
     $('<h4>').html('<span>capital </span>of ' + item.name + ' is <span> ' + item.capital +'</span>').appendTo(countriesList);
-    $('<li>').html('<span>population: </span> ' + item.population + ' people').appendTo(countriesList);
+    $('<li>').html('<span>population: </span> ' + item.population/1000000 + ' mln people').appendTo(countriesList);
     $('<li>').html('<span>timezone:</span> ' + item.timezones).appendTo(countriesList);
     $('<li>').html('<span>land area:</span> '+ item.area + ' km²').appendTo(countriesList);
     $('<li>').html('<span>currencies:</span> '+ item.currencies).appendTo(countriesList);
