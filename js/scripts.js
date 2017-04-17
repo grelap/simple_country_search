6bf25d24ac24e8c85c4dbf66d88f5b003813a06e
xenogonx <pablez@wp.pl>
2017-04-17 20:18:06 +0200

    API

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+var url = 'https://restcountries.eu/rest/v1/name/'; 
+var countriesList = $('#countries');
+
+$('#search').click(searchCountries);
+
+function searchCountries() {
+  var countryName = $('#country-name').val(); if(!countryName.length) countryName = 'Poland';
+  $.ajax({
+    url: url + countryName, 
+    method: 'GET', 
+    success: showCountriesList }); 
+}
+function showCountriesList(resp) {
+  countriesList.empty();
+  resp.forEach(function(item) 
+  { 
+  
+    $('<h3>').text(item.name).appendTo(countriesList);
+    $('<img>').attr('src', '' + 'http://www.geonames.org/flags/x/' + item.alpha2Code.toLowerCase() + ".gif").appendTo(countriesList);
+    $('<h4>').html('<span>capital </span>of ' + item.name + ' is <span> ' + item.capital +'</span>').appendTo(countriesList);
+    $('<li>').html('<span>population: </span> ' + item.population + ' people').appendTo(countriesList);
+    $('<li>').html('<span>timezone:</span> ' + item.timezones).appendTo(countriesList);
+    $('<li>').html('<span>land area:</span> '+ item.area + ' km²').appendTo(countriesList);
+    $('<li>').html('<span>code: </span>' + item.alpha2Code).appendTo(countriesList);
+    
+     
+  });        
+
+ }
